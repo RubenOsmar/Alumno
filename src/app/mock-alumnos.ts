d9f9d2eab03e77eab76b69fd2c89f06b5690b9b5
RubenOsmar <ruben_osmar19@hotmail.com>
2019-09-04 08:45:17 -0500

    Crea el archivo src/app/mock-alumnos.ts

---
+++ src/app/mock-alumnos.ts
@@ -0,0 +1,3 @@
+
+import { Alumno } from 'src/alumno';
+export const ALUMNOS: Alumno[] = [ { id: 11, name: 'Michael' }, { id: 12, name: 'Marco' }, { id: 13, name: 'David' }, { id: 14, name: 'Maria' }, { id: 15, name: 'Alberto' }, { id: 16, name: 'Alexis' }, { id: 17, name: 'Nayeli' }, { id: 18, name: 'Jesus' }, { id: 19, name: 'Andres' }, { id: 20, name: 'Issac' } ];
