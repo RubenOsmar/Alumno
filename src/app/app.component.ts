2dbadf3d6eeae6a4dc062a56fcac3fbca24f4457
RubenOsmar <ruben_osmar19@hotmail.com>
2019-09-04 08:38:16 -0500

    Verifica que este correcto src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Alumnos del TESE';
+  title = 'Galeria de Alumnos del TESE';
 }
